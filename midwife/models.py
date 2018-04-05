590c8f45cd181d08ff0da50418ea14036817fd04
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-04-05 11:41:59 +0300

    add user model with its field

--- midwife/models.py
+++ midwife/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=60, null=True)
+    type_of_user = models.CharField(max_length=30, null=True)
+    national_id= models.IntegerField(max_length=40, null=True)
+    phonenumber= models.CharField(max_length=60)
+    location = models.CharField(max_length=60, null=True)
+    nearest_town = models.CharField(max_length=50)
