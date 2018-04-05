8e7de390bcdf8b8237702ffb977b9148d5a17175
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-04-05 11:56:59 +0300

    add session model

--- midwife/models.py
+++ midwife/models.py
@@ -8,3 +8,22 @@ class User(models.Model):
     phonenumber= models.CharField(max_length=60)
     location = models.CharField(max_length=60, null=True)
     nearest_town = models.CharField(max_length=50)
+
+    @classmethod
+    def requested_users(cls, user_type):
+        '''
+        Method that gets list of farmers for buyer and vice versa
+        '''
+        if user_type == '1':
+            requested_type = '2'
+        else:
+            requested_type = '1'
+
+        found_users = User.objects.filter(type_of_user=requested_type)
+        return found_users
+
+class session_levels(models.Model):
+	session_id = models.CharField(max_length=25,primary_key=True)
+	phonenumber= models.CharField(max_length=25,null=True)
+	level = models.IntegerField(null=True)
+
