88b0db586264b5f92c2da21d32f73f4639e09d3a
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-04-05 11:43:47 +0300

    make migrations for user model

--- midwife/models.py
+++ midwife/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=60, null=True)
     type_of_user = models.CharField(max_length=30, null=True)
-    national_id= models.IntegerField(max_length=40, null=True)
+    national_id= models.IntegerField(null=True)
     phonenumber= models.CharField(max_length=60)
     location = models.CharField(max_length=60, null=True)
     nearest_town = models.CharField(max_length=50)
